7a97821669b16d7201cfb81e3d65a59dd4aa45a4
BugZ-EKB <e.shorohov87@yandex.ru>
2023-08-20 19:55:09 +0400

    Solution 1

--- tasks/task.py
+++ tasks/task.py
@@ -1,10 +1,19 @@
+import functools
 
-def validate():
-    '''
-    Add corresponded arguments and implementation here. 
-    '''
-    pass 
+def validate(func):
+    @functools.wraps(func)
+    def wrapper (x,y,z):
+        if 0 <= x <= 256 and 0 <= y <= 256 and 0 <= z <= 256:
+            result = func(x,y,z)
+        else:
+            result = f'Function call is not valid!'
+        return result
+    return wrapper
+    pass
 
-@validate
-def set_pixel(x: int, y: int, z: int) -> str:
-  return "Pixel created!"
+# @validate
+# def set_pixel(x: int, y: int, z: int) -> str:
+#   return "Pixel created!"
+#
+# print(set_pixel(0, 127, 300))
+# print(set_pixel(0, 127, 255))
